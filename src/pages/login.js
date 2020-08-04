0d73ca553ab81da98192572cc9febc829bf40c21
RinaLia <loa.orlando@gmail.com>
2020-08-05 00:09:15 +0700

    init

---
+++ src/pages/login.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import {
+  Row,
+  Col,
+  Form,
+  Button,
+  FormGroup,
+  FormLabel,
+  FormControl,
+  Card,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+class login extends Component {
+  render() {
+    return (
+      <>
+        <Row className="h-100 no-gutters">
+          <Col xs={6} md={6} lg={6} className="bg-login">
+            <div className="bg-text-login">
+              <h1 className="text-white mt-3">Book is a window to the world</h1>
+            </div>
+          </Col>
+          <Col xs={6} md={6} lg={6}>
+            <div className="d-flex flex-column w-100 h-100">
+              <div className=" text-center mt-3">
+                <h1>Login</h1>
+              </div>
+              <div className="greeting text-center mt-4 ">
+                <h4>Hello, friend!</h4>
+                <p>
+                  Enter your personal details and start your reading journey
+                  with us!
+                </p>
+              </div>
+              <div className=" h-80 d-flex mt-2 justify-content-center align-items-center">
+                <div className="card-login d-flex justify-content-center mt-2 ml-2">
+                  <Form>
+                    <FormGroup controlId="formBasicEmail">
+                      <FormLabel>Email address</FormLabel>
+                      <FormControl type="email" placeholder="Enter email" />
+                    </FormGroup>
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                      Submit
+                    </Button>
+                    <p className="mt-3">Don't have account? Please register</p>
+                  </Form>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </>
+    );
+  }
+}
+
+export default login;
